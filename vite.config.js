3d917166719a3af026306db064b039db77f71b12
Ahmed Hanye <ahmedhanye161004@gmail.com>
2024-03-17 05:29:11 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import csp from "vite-plugin-csp";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    csp({
+      // Specify the CSP directives here
+      directives: {
+        "object-src": `'none'`,
+        "base-uri": `'self'`,
+      },
+    }),
+  ],
   base: "/ip-address-tracker/",
 });
